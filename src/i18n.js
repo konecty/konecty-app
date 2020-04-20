4387d5eb2ad04b4ba6068e81ceb558104d08e44d
Derotino Silveira <silveirado@gmail.com>
2020-04-20 19:52:55 -0300

    Enable CI

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,14 @@
 import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
+import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(Backend)
+i18n.use(HttpApi)
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
+		loadPath: 'locales/{{lng}}/{{ns}}.json',
+		addPath: 'locales/add/{{lng}}/{{ns}}',
 		fallbackLng: 'pt-BR',
 		debug: true,
 		interpolation: {
