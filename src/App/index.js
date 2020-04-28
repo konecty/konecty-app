94b6bb35f005d9811d611a08b7f54b62f5b0f17f
Leonardo Viva <leonardo.leal@konecty.com>
2020-04-28 13:12:39 -0300

    Add package.json

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,22 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import luxonAdapter from '@date-io/luxon';
+
 import Routes from '../Routes';
 import Loader from '../Components/Loader';
 
 const App = () => {
 	return (
-		<>
+		<MuiPickersUtilsProvider utils={luxonAdapter}>
 			<CssBaseline />
 			<Suspense fallback={<Loader />}>
 				<Router>
 					<Routes />
 				</Router>
 			</Suspense>
-		</>
+		</MuiPickersUtilsProvider>
 	);
 };
 
