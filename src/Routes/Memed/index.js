b27ab07efb9f2382b8ba0cc78d45825308cf1d8e
Marcelo Mota Ribeiro <marcelomotaribeiro@outlook.com.br>
2020-05-28 11:12:10 -0300

    Set progress and timeout on load memed

--- src/Routes/Memed/index.js
+++ src/Routes/Memed/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const Memed = () => {
 	const memedUrlParams = new URLSearchParams(window.location.search);
@@ -6,6 +9,25 @@ const Memed = () => {
 	const memedHost = memedUrlParams.get('host');
 	const memedPacienteNome = memedUrlParams.get('nome');
 	const memedPacienteTelefone = memedUrlParams.get('telefone');
+
+	const [showTimeoutAlert, setShowTimeoutAlert] = useState(false)
+
+	let memedCheckTimeoutCount = 0
+	const memedCheckTimeout = setInterval(() => {
+		if (document.querySelector('#memed-auto-generated') == null) {
+			memedCheckTimeoutCount++;
+			if (memedCheckTimeoutCount >= 15) {
+				setShowTimeoutAlert(true);
+				clearInterval(memedCheckTimeout);
+			}
+		} else {
+			clearInterval(memedCheckTimeout);
+		}
+	}, 1000);
+	
+	const memedPopUpClose = () => {
+		window.close();
+	}	
 
 	const memedScriptImported = () => {
 		const scriptsOnDocument = document.getElementsByTagName('script');
@@ -60,11 +82,18 @@ const Memed = () => {
 	}
 
 	return (
-		<div>
-			<a id="memed-prescricao" onClick={memedAntesDeClicarBotaoPrescricao} style={{ display: 'none' }}>
-				Click-me
-			</a>
-		</div>
+        <div style={{
+			position: 'absolute', left: '50%', top: '50%',
+			transform: 'translate(-50%, -50%)'
+		}}>
+			<CircularProgress style={{display: showTimeoutAlert ? 'none' : ''}} size={40} thickness={4} value={100} />
+			<div>
+				<Typography style={{display: showTimeoutAlert ? '' : 'none'}} variant="subtitle1">Ops! Não foi possível carregar o módulo de prescrições da Memed.</Typography><br />
+				<Typography style={{display: showTimeoutAlert ? '' : 'none'}}  variant="subtitle1">Aguarde alguns instantes e tente novamente!</Typography><br />
+				<Button style={{display: showTimeoutAlert ? '' : 'none'}}  onClick={memedPopUpClose} variant="outlined" color="primary">Fechar</Button>				
+			</div>
+            <a id="memed-prescricao" onClick={memedAntesDeClicarBotaoPrescricao} style={{display: 'none'}}>Click-me</a>
+        </div>
 	);
 };
 
